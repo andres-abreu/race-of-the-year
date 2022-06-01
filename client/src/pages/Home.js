3ab636b473984486d0815e189983f6a500d00148
Maria Andrea Rodriguez <mandrea.rodriguezf@gmail.com>
2022-06-01 16:48:21 -0400

    disclaimer

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -17,6 +17,21 @@ const Home = () => {
                 <source src="public/images/runners.jpg" type=""></source>
             </video>
         </section>
+
+        <div class="card">
+  <div class="card-header text-center text-danger">DISCALIMER</div>
+  <div class="card-body">
+    <blockquote class="blockquote mb-0 text-center">
+      <p>
+      All participants in joinder with this run-marathon-contract are with the knowledge and acceptance of the Terms and Conditions.  
+      Run of The Year Marathon will not be liable for any damages, accidents, weight-loss, smelly-clothes during the race and as-well-as mis-matched-shoes or mis-sized clothing from our online-store-orders.
+      <br></br> 
+      All participants will adhere to an arbitration in the event of a valid claim, please contact Shwarzt & Shwartz & Spielburger & Fries law-firm and ask for Sal. Have a great run! 
+      </p>
+      <footer class="blockquote-footer">The Race of The Year Team</footer>
+    </blockquote>
+  </div>
+</div>
         </>
     )
 }
