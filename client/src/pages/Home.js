4efaf784fde9aae9f5a0e8059cb8e9cb94358ba4
Andres: Abreu <andresabreu@andress-MacBook-Pro.local>
2022-06-01 17:50:13 -0400

    footer-mod

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
             <div classNameName="container d-flex flex-column">
                 <h1 classNameName="text-center "></h1>
                 <p></p>  
-                <img src="/images/runners.jpg" className='homephoto img-fluid'/>
+                <img src="/images/kidsrun.jpeg" className='homephoto img-fluid'/>
             </div>
                 
         </section>
@@ -23,9 +23,8 @@ const Home = () => {
     <blockquote class="blockquote mb-0 text-center">
     <img src="/images/firemyattorney.jpg" className="img-fluid"/>
       <p>
-      All participants in joinder with this run-marathon-contract are with the knowledge and acceptance of the Terms and Conditions.  
+      All participants in joinder with this run-marathon-contract are with the knowledge and acceptance of the Terms and Conditions.
       Run of The Year Marathon will not be liable for any damages, accidents, weight-loss, smelly-clothes during the race and as-well-as mis-matched-shoes or mis-sized clothing from our online-store-orders.
-      <br></br> 
       All participants will adhere to an arbitration in the event of a valid claim, please contact Shwarzt & Shwartz & Spielburger & Fries law-firm and ask for Sal. Have a great run! 
       </p>
       <footer class="blockquote-footer">The Race of The Year Team</footer>
