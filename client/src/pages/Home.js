69d0f1e9aae90f7ce7c9f7432bd6862971d8d1b0
Maria Andrea Rodriguez <mandrea.rodriguezf@gmail.com>
2022-06-01 19:13:13 -0400

    added blog post

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -13,9 +13,7 @@ const Home = () => {
                 <h1 classNameName="text-center "></h1>
                 <p></p>  
             </div>
-            <video autoPlay muted loop id="">
-                <source src="public/images/runners.jpg" type=""></source>
-            </video>
+            
         </section>
 
         <div class="card text-center">
@@ -27,6 +25,34 @@ const Home = () => {
               Commemorate your experience with official gear, anniversary swag, or prep for 2023 with In-Training apparel!.</p>
              <a href="#" class="btn btn-primary">Shop</a>
          </div>
+
+         <section>
+        <div class="card mb-3"><div class="card-header text-center text-danger"></div>
+        <h5 class="card-title">SUMMER RUNNING TIPS</h5>
+  <img src="images/summer-running.jpg" alt="woman running in the afternoon" class="card-img-top" alt="Wild Landscape"/>
+  <div class="card-body">
+    
+    <p class="card-text text-justify">
+      
+          1. Run by effort, not pace.
+          <br></br>
+          2. Run when the termperature is the coolest. Early AM or late PM.
+          <br></br>
+          3. Hydrate before, during and after your run.
+          <br></br>
+          4. Try to run in the shade as much as possible.
+          <br></br>
+          5. Wear breathable, light clothing to keep cool.
+      
+    </p>
+    <p class="card-text">
+      <small class="text-muted">Last updated June 1, 2022</small>
+    </p>
+  </div>
+</div>
+        </section>
+
+
         <section>
             <div>
                 <p></p>
