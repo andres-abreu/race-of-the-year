1fabbe85e432c4dc590846ba88365d9ab95cf837
Maria Andrea Rodriguez <mandrea.rodriguezf@gmail.com>
2022-06-01 19:41:48 -0400

    corrected typos

--- client/src/components/RaceMap/index.js
+++ client/src/components/RaceMap/index.js
@@ -13,6 +13,8 @@ const RaceMap = () => {
                 <p></p>
             </div>
         </section>
+        
+
         <div class="row row-cols-1 row-cols-md-2 g-4">
   <div class="col">
     <div class="card">
@@ -30,6 +32,7 @@ const RaceMap = () => {
       </div>
     </div>
   </div>
+  
   <div class="col">
     <div class="card">
       <img src="images/registration.jpg" class="card-img-top" alt="Registration form"/>
